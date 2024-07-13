d6aed63a92d181970abc8c1d83333d9780d78c6f
0xkanekiken <100861945+0xKanekiKen@users.noreply.github.com>
2024-07-13 07:47:03 +0000

    feat: add put option pricing
    Signed-off-by: 0xkanekiken <100861945+0xKanekiKen@users.noreply.github.com>

--- program/src/main.rs
+++ program/src/main.rs
@@ -9,6 +9,16 @@
 sp1_zkvm::entrypoint!(main);
 
 use std::f64::consts::PI;
+use serde::{Serialize, Deserialize};
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct BlackScholesInput {
+    pub price: f64,
+    pub strike: f64,
+    pub iv: f64,
+    pub time: f64,
+    pub rate: f64,
+}
 
 fn norm_pdf(x: f64) -> f64 {
     (-x * x / 2.0).exp() / (2.0 * PI).sqrt()
@@ -35,47 +45,27 @@ fn call_price(s: f64, k: f64, r: f64, sigma: f64, t: f64) -> f64 {
     s * norm_cdf(d1) - k * (-r * t).exp() * norm_cdf(d2)
 }
 
+fn put_price(s: f64, k: f64, r: f64, sigma: f64, t: f64) -> f64 {
+    let d1 = d1(s, k, r, sigma, t);
+    let d2 = d2(s, k, r, sigma, t);
+    k * (-r * t).exp() * norm_cdf(-d2) - s * norm_cdf(-d1)
+}
+
 pub fn main() {
-    let s = 100.0; // Current stock price
-    let k = 100.0; // Strike price
-    let r = 0.05;  // Risk-free rate
-    let sigma = 0.2; // Volatility
-    let t = 1.0;   // Time to expiration in years
+    let option_input = sp1_zkvm::io::read::<BlackScholesInput>();
+
+    let s = option_input.price; // Current stock price
+    let k = option_input.strike; // Strike price
+    let r = option_input.rate;  // Risk-free rate
+    let sigma = option_input.iv; // Volatility
+    let t = option_input.time;   // Time to expiration in years
 
     let call = call_price(s, k, r, sigma, t);
-    // let put = put_price(s, k, r, sigma, t);
+    let put = put_price(s, k, r, sigma, t);
 
     println!("Call option price: {:.4}", call);
-    // println!("Put option price: {:.4}", put);
+    println!("Put option price: {:.4}", put);
 
     // sp1_zkvm::io::write(&call);
     // sp1_zkvm::io::write(&b);
 }
-
-// pub fn main() {
-//     // Read an input to the program.
-//     //
-//     // Behind the scenes, this compiles down to a custom system call which handles reading inputs
-//     // from the prover.
-//     let n = sp1_zkvm::io::read::<u32>();
-
-//     // Write n to public input
-//     sp1_zkvm::io::commit(&n);
-
-//     // Compute the n'th fibonacci number, using normal Rust code.
-//     let mut a = 0;
-//     let mut b = 1;
-//     for _ in 0..n {
-//         let mut c = a + b;
-//         c %= 7919; // Modulus to prevent overflow.
-//         a = b;
-//         b = c;
-//     }
-
-//     // Write the output of the program.
-//     //
-//     // Behind the scenes, this also compiles down to a custom system call which handles writing
-//     // outputs to the prover.
-//     sp1_zkvm::io::commit(&a);
-//     sp1_zkvm::io::commit(&b);
-// }
