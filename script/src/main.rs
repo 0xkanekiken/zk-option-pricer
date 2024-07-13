6788a802a6785c27a47048abd5617a541e5eb283
0xkanekiken <100861945+0xKanekiKen@users.noreply.github.com>
2024-07-13 08:31:07 +0000

    feat: make std input work
    Signed-off-by: 0xkanekiken <100861945+0xKanekiKen@users.noreply.github.com>

--- script/src/main.rs
+++ script/src/main.rs
@@ -1,14 +1,39 @@
 use sp1_sdk::{utils, ProverClient, SP1Proof, SP1Stdin};
+use serde::{Serialize, Deserialize};
 
 /// The ELF we want to execute inside the zkVM.
 const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct BlackScholesInput {
+    pub price: f64,
+    pub strike: f64,
+    pub iv: f64,
+    pub time: f64,
+    pub rate: f64,
+}
+
+impl Default for BlackScholesInput {
+    fn default() -> Self {
+        BlackScholesInput {
+            price: 100.0,
+            strike: 105.0,
+            iv: 0.2,
+            time: 1.0,
+            rate: 0.05,
+        }
+    }
+}
 
 fn main() {
     // Generate proof.
     // utils::setup_tracer();
     utils::setup_logger();
 
-    let stdin = SP1Stdin::new();
+    let input = BlackScholesInput::default();
+    let mut stdin = SP1Stdin::new();
+    stdin.write(&input);
+
     let client = ProverClient::new();
     let (pk, vk) = client.setup(ELF);
     let proof = client.prove(&pk, stdin).expect("proving failed");
